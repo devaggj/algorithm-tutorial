195feb8aba986b761634bfe352682cb8b33bce53
geniefor <redjin153@gmail.com>
2021-11-20 17:12:12 +0900

    .

--- src/hello.py
+++ src/hello.py
@@ -1,13 +1,8 @@
 class Hello:
 
     def run(self):
-<<<<<<< HEAD
-        print('Hello World!');
-        print('Meet Atom.');
-=======
         #print('Hello World!');
         print('Hello Atom.');
->>>>>>> 1112131b126aa5e20bfbac6c14036b88f1619367
 
 def main():
     Hello().run();
