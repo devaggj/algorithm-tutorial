6a2b45e4c430606fd72427f39da9e2fb4664934e
geniefor <redjin153@gmail.com>
2022-01-18 00:31:42 +0900

    ...

--- src/programmers_coding_practice/level_02/08_slicing_n^2_arr.py
+++ src/programmers_coding_practice/level_02/08_slicing_n^2_arr.py
@@ -1,6 +1,6 @@
 """
 :link: https://programmers.co.kr/learn/courses/30/lessons/87390?language=python3
-:ref:
+:ref: https://velog.io/@front/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-n2-%EB%B0%B0%EC%97%B4-%EC%9E%90%EB%A5%B4%EA%B8%B0
 """
 
 def solution(n, left, right):
